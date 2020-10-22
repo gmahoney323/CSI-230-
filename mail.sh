157cee7d34b7f46a4c4932225e4b5ea91de22034
gmahoney323 <grant.mahoney@mymail.champlain.edu>
2020-10-22 16:17:02 -0400

    push2

--- mail.sh
+++ mail.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 filename='emails.txt'
+groupname=CSI230
+shellpath=/bin/bash
 
 if [ $EUID -ne 0 ]; then
 	echo "Must Run as Root"
 	exit 2
 fi
 
-getUsername()
+listUsername()
 {
   while read j
   do
@@ -15,10 +17,46 @@ getUsername()
   done < $filename
 }
 
+getEmailAddress()
+{
+  line=$1
+  sed "${line}q;d" $filename
+}
+
+getUsername()
+{
+  line=$1
+  sed "${line}q;d" $filename | cut -d "@" -f 1
+}
+
 createRandomPassword()
 {
   openssl rand -base64 12 | tr -d =+/
 }
 
-echo $(getUsername)
+createUser()
+{
+  usern=$1
+  pass=$2
+  email=$3
+  #command on $usern
+  #echo "username is ${usern}"
+  #echo "password is ${pass}"
+
+  exists=`grep -c ${usern}: /etc/passwd`
+  echo "existence status: ${exists}"
+  #checks if user does not exist
+  if [ $exists = 0 ]; then
+    useradd -m -p $pass -s $shellpath -g $groupname $usern
+    chage -d 0 $usern
+  else
+    echo "reset password"
+    passwd $usern
+  fi
+  #echo "new user ${usern} created with id" gid $usern
+}
+
+echo $(listUsername)
 echo $(createRandomPassword)
+echo $(createUser $(getUsername 1) $(createRandomPassword) $(getEmailAddress 1))
+echo $(getUsername 2)
